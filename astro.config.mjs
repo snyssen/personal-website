62067b752254f4dfa4246d55d1b654797080af6d
Simon Nyssen <dev@snyssen.be>
2023-01-17 11:12:43 +0100

    Add @astrojs/image to auto-optimize hero images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.WEBSITE_URI ?? 'http://localhost:3000',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: process.env.WEBSITE_URI ?? "http://localhost:3000",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
 });
