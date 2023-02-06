990c38c2c8f54823f33b622ea46ae5cfa4bf8944
Simon Nyssen <dev@snyssen.be>
2023-02-06 11:22:51 +0100

    Prevent plausible script from loading when generating PDF

--- scripts/generate-pdf.ts
+++ scripts/generate-pdf.ts
@@ -6,6 +6,12 @@ import { chromium } from 'playwright';
     const browser = await chromium.launch();
     const page = await browser.newPage();
 
+    // Remove deferred loading of plausible script because it is blocked in CI
+    await page.route('https://plausible.snyssen.be/**', async route => {
+        console.debug('Aborting route', route.request().url());
+        await route.abort();
+    });
+
     await page.goto('http://localhost:3000/resume', { waitUntil: 'load', timeout: 60 * 1000 });
 
     console.log('Resume page loaded, removing components...');
