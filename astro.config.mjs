f2cf44824e9abf16dc883e74afc3cb3d96981692
Simon Nyssen <dev@snyssen.be>
2023-12-24 17:16:00 +0100

    refactor(blog): remove lunr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,32 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import astroLunr from "@siverv/astro-lunr/plugin.mjs";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
   site: process.env.WEBSITE_URI ?? "http://localhost:4321",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    astroLunr({
-      subDir: "lunr",
-      initialize: (builder, lunr) => {
-        builder.field("title", {
-          boost: 10,
-        });
-        builder.field("description", {
-          boost: 5,
-        });
-        builder.field("tag", {
-          boost: 8,
-        });
-        builder.field("content", {
-          boost: 1,
-        });
-      },
-    }),
-    preact(),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), preact()],
 });
