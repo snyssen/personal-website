202e64103957d78c0b7a7a53a522762257483856
Simon Nyssen <dev@snyssen.be>
2024-01-23 22:49:33 +0100

    feat(blog): add icon after external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,33 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import expressiveCode from "astro-expressive-code";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.WEBSITE_URI ?? "http://localhost:4321",
-  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), preact()],
+  integrations: [
+    expressiveCode(),
+    mdx({ rehypePlugins: [] }),
+    sitemap(),
+    tailwind(),
+    preact(),
+  ],
   image: {
     domains: ["httpgoats.com"],
+  },
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: {
+            type: "raw",
+            value:
+              ' <i class="las la-external-link-alt text-sm" aria-hidden="true"></i>',
+          },
+        },
+      ],
+    ],
   },
 });
